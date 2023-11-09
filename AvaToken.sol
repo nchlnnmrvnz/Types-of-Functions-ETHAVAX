92df75fcc87c9e76956f909b23dda97002eb59de
nchlnnmrvnz <114238074+nchlnnmrvnz@users.noreply.github.com>
2023-11-10 00:37:00 +0800

    ADD project file

---
+++ AvaToken.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AvaToken is ERC20 {
+    address public owner;
+
+    constructor(uint256 initialSupply) ERC20("Ava", "AVA") {
+        _mint(msg.sender, initialSupply);
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Error: You are not the owner");
+        _;
+    }
+
+    function mint(address to, uint256 value) external onlyOwner {
+        _mint(to, value);
+    }
+
+    function burn(address from, uint256 value) external {
+        _burn(from, value);
+    }
+
+    function transfer(address to, uint256 value) public override returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+}
